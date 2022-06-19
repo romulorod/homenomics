a258037e120621c7905e6bfc51e0d7f614e77402
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-06-18 23:27:17 -0300

    Refactor: procedural code instead of imperative html

--- components/SelectMonth/SelectMonth.js
+++ components/SelectMonth/SelectMonth.js
@@ -1,44 +1,27 @@
 import React from 'react'
 
 export default function SelectMonth({ month, setMonth }) {
+  const months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ]
   return (
     <select data-testid="select" value={month} onChange={(e) => setMonth(e.target.value)}>
-      <option data-testid="select-option" value="jan">
-        Janeiro
-      </option>
-      <option data-testid="select-option" value="fev">
-        Fevereiro
-      </option>
-      <option data-testid="select-option" value="mar">
-        Março
-      </option>
-      <option data-testid="select-option" value="apr">
-        Abril
-      </option>
-      <option data-testid="select-option" value="mai">
-        Maio
-      </option>
-      <option data-testid="select-option" value="jun">
-        Junho
-      </option>
-      <option data-testid="select-option" value="jul">
-        Julho
-      </option>
-      <option data-testid="select-option" value="ago">
-        Agosto
-      </option>
-      <option data-testid="select-option" value="set">
-        Setembro
-      </option>
-      <option data-testid="select-option" value="out">
-        Oututubro
-      </option>
-      <option data-testid="select-option" value="nov">
-        Novembro
-      </option>
-      <option data-testid="select-option" value="dez">
-        Dezembro
-      </option>
+      {months.map((month) => (
+        <option data-testid="select-option" key={month} value={month}>
+          {month}
+        </option>
+      ))}
     </select>
   )
 }
