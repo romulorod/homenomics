254cb0e60f6d3ad0fc23f808071f42d91db2fa5f
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-06-18 22:47:36 -0300

    Feat > Create title tests

---
+++ components/Title/index.spec.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+import { fireEvent, render, screen } from '@testing-library/react'
+import Title from './Title'
+import '@testing-library/jest-dom'
+
+describe('Render title', () => {
+  it('should render Title', () => {
+    render(<Title />)
+    expect(screen.getByText(/Homenomics/)).toBeTruthy()
+  })
+
+  it('Should render SVG pie chart', () => {
+    render(<Title />)
+    const img = screen.getByAltText(/Pie chart/i)
+    expect(screen.getByRole('img')).toBeTruthy()
+  })
+})
